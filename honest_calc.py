8e92cba7af2caf1d28a0752979680005a700f987
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-10 12:54:44 +0100

    Stage 1/5: Data collection

---
+++ honest_calc.py
@@ -0,0 +1,25 @@
+import ast
+
+msg_0 = 'Enter an equation'
+msg_1 = 'Do you even know what numbers are? Stay focused!'
+msg_2 = 'Yes ... an interesting math operation. You\'ve slept through all ' \
+        'classes, haven\'t you?'
+valid_operations = {'+', '-', '*', '/'}
+
+while True:
+    print(msg_0)
+    calc = input()
+    x, oper, y = calc.split()
+
+    try:
+        x = ast.literal_eval(x)
+        y = ast.literal_eval(y)
+    except ValueError:
+        print(msg_1)
+        continue
+
+    if oper not in valid_operations:
+        print(msg_2)
+        continue
+
+    break
