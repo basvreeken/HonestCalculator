d74bad146e7fa8a692b7a6c22fd148a839b4553e
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-10 20:29:02 +0100

    Stage 3/5: Total recall

--- honest_calc.py
+++ honest_calc.py
@@ -5,12 +5,22 @@ msg_1 = 'Do you even know what numbers are? Stay focused!'
 msg_2 = 'Yes ... an interesting math operation. You\'ve slept through all ' \
         'classes, haven\'t you?'
 msg_3 = 'Yeah... division by zero. Smart move...'
+msg_4 = 'Do you want to store the result? (y / n):'
+msg_5 = 'Do you want to continue calculations? (y / n):'
 valid_operations = {'+', '-', '*', '/'}
+memory = float(0)
+play_again = False
+skip_check = False
 
 while True:
     print(msg_0)
     calc = input()
     x, oper, y = calc.split()
+
+    if x == 'M':
+        x = str(memory)
+    if y == 'M':
+        y = str(memory)
 
     try:
         x = ast.literal_eval(x)
@@ -35,6 +45,33 @@ while True:
         except ZeroDivisionError:
             print(msg_3)
             continue
-    break
 
-print(float(result))
+    print(float(result))
+
+    while True:
+        print(msg_4)
+        answer = input()
+        if answer == 'y':
+            memory = result
+            break
+        elif answer == 'n':
+            break
+        else:
+            continue
+
+    while True:
+        print(msg_5)
+        answer = input()
+        if answer == 'y':
+            play_again = True
+            break
+        elif answer == 'n':
+            play_again = False
+            break
+        else:
+            continue
+
+    if play_again:
+        continue
+    else:
+        break
