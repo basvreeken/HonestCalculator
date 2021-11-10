5cfaefd210c097b3ea1d16fe8854ee41850fa419
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-11-10 19:19:03 +0100

    Stage 2/5: First calculations

--- honest_calc.py
+++ honest_calc.py
@@ -4,6 +4,7 @@ msg_0 = 'Enter an equation'
 msg_1 = 'Do you even know what numbers are? Stay focused!'
 msg_2 = 'Yes ... an interesting math operation. You\'ve slept through all ' \
         'classes, haven\'t you?'
+msg_3 = 'Yeah... division by zero. Smart move...'
 valid_operations = {'+', '-', '*', '/'}
 
 while True:
@@ -22,5 +23,18 @@ while True:
         print(msg_2)
         continue
 
+    if oper == '+':
+        result = x + y
+    elif oper == '-':
+        result = x - y
+    elif oper == '*':
+        result = x * y
+    elif oper == '/':
+        try:
+            result = x / y
+        except ZeroDivisionError:
+            print(msg_3)
+            continue
     break
 
+print(float(result))
